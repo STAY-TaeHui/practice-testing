3d3145717a1ecc71e24591b2664a711e7f69f8c7
서태희 <154861922+thseo-e@users.noreply.github.com>
2024-04-23 20:04:06 +0900

    20~21강 - Persistence Layer Test - @DataJpaTest

--- cafekiosk/src/main/java/com/example/cafekiosk/spring/domain/product/Product.java
+++ cafekiosk/src/main/java/com/example/cafekiosk/spring/domain/product/Product.java
@@ -3,6 +3,7 @@ package com.example.cafekiosk.spring.domain.product;
 import com.example.cafekiosk.spring.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,4 +30,13 @@ public class Product extends BaseEntity
 
     private int price;
 
+    @Builder
+    public Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price)
+    {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
